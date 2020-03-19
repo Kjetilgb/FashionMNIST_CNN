8455e783b626824cd77d780491fafbac44edff38
Kjetil Granaune Brenne <kjetil@Kjetils-MacBook-Pro.local>
2020-03-19 22:50:43 +0100

    "More changes"

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
+import cv2
 import torch
+import torchvision
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
+import numpy as np
+import matplotlib.pyplot as plt
+
+from torch.autograd import Variable
+from PIL import Image
 
 transform = transforms.Compose(
-    [transforms.ToTensor()]
+    [transforms.RandomResizedCrop(64),
+    transforms.ToTensor()]
 )
 
-BATCH_SIZE = 32
+BATCH_SIZE = 100
 
 train_set = dset.FashionMNIST(root='./data', train=True, 
                                 download=True, transform=transform)
 
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=BATCH_SIZE, 
-                                            shuffle=True, num_workers=2)
+                                            shuffle=True)
 
 test_set = dset.FashionMNIST(root='./data', train=False, 
                                 download=True, transform=transform)
 
 test_loader = torch.utils.data.DataLoader(test_set, batch_size=BATCH_SIZE, 
-                                            shuffle=False, num_workers=2)
+                                            shuffle=False)
 
-print(train_set)
+def img_show(img):
+    img = img / 2 + 0.5
+    npimg = img.numpy()
+    plt.figure(figsize=(20,20))
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
 
+dataiter = iter(train_loader)
+images, labels = dataiter.next()
+
+img_show(torchvision.utils.make_grid(images))
 # classes = trainset.classes
 ## input shape (width=28, height=28, depth=1)
---
+++ readMe.md
@@ -0,0 +1,7 @@
+## Oblig 2 - Convolutional Neural Network
+### Zib007 - Kjetil G. Brenne
+
+This is an obligatory assignment for INFO284 - Machine Learning at UiB. The main purpose of the assigment is to build a convolutional neural network with PyTorch on the Fashion MNIST dataset (https://github.com/zalandoresearch/fashion-mnist).
+
+The goal of the project is to learn how to build a CNN for image classification and learn the basics of building and training a neural network
+
